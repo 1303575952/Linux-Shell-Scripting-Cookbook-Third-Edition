cc2fdf3aa5a1891811bcbf99bf0aada6dfd15ff1
1303575952 <1303575952@qq.com>
2019-10-10 14:41:45 +0800

    Chapter01

---
+++ Chapter01/time_take.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# 文件名：time_take.sh
+
+start=$(date +%s)
+commands;
+statements;
+end=$(date +%s)
+difference=$(( end - start ))
+echo Time taken to execute commands is $difference seconds.
